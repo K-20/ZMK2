a3f9bedc8019eae8d39e70a68ceac83f35ef6362
K-20 <xalone2206@gmail.com>
2025-09-28 16:34:08 +0700

    Delete config directory

--- config/boards/arm/noru64/board.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2020 The ZMK Contributors
-# SPDX-License-Identifier: MIT
-#
-
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softdevice")
-board_runner_args(jlink "--device=nrf52" "--speed=4000")
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
